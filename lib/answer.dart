14b0cda9057727014081fdc8bde8c6d0bab98c90
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-09-10 13:56:27 +0000

    Use lifting technique to pass function down to other widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
+  final selectHandler;
+
+  Answer(this.selectHandler);
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: ElevatedButton(
         child: Text('Answer1'),
-        onPressed: null,
+        onPressed: selectHandler,
       ),
     );
   }
