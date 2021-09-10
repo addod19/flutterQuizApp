105d477dc3ce227307365223e3dfbcb0d7db1c8d
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2021-09-10 10:05:33 +0000

    Add column widget and a couple of Button and Text widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your fav color?',
+      'Whats\s your fav programming language?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first flutter App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Text('The question!!!'),
+            ElevatedButton(
+              child: Text('Answer1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Answer3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
